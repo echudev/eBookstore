8fbcf5296b0f30e2b3c39a9c2f98cb41e13a6935
Ezequiel <ezequiel.maranda@gmail.com>
2023-08-14 00:36:20 -0300

    feat: framer-motion added to navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
 import { useStore } from "@/store/useReadingList";
+import { LayoutGroup, motion } from "framer-motion";
 
 export default function Navbar() {
   const { readingList, toggleBook } = useStore();
@@ -41,32 +42,38 @@ export default function Navbar() {
       <h1 className="relative text-3xl mb-5">
         <b className="text-violet-500">e</b>Bookstore
       </h1>
-      <div className="flex gap-6 mt-2">
-        {navLinks.map((link, i) => {
-          let isActive = pathname === link.href;
-          return (
-            <Link
-              key={i}
-              href={link.href}
-              className="relative hover:bg-white/10 rounded transition-all py-2 px-3"
-            >
-              <p className={isActive ? "" : "text-white/70"}>{link.name}</p>
-              {link.id === 1 && link.books !== null && link.books > 0 && (
-                <div className="absolute top-1 -right-1 bg-yellow-300 text-neutral-800 font-bold rounded-full w-4 h-4 flex items-center justify-center text-xs">
-                  {link.books}
-                </div>
-              )}
-              <div
-                className={
-                  isActive
-                    ? "absolute inset-0 border-b-2 border-yellow-300 z-[-1]"
-                    : "absolute inset-0 border-b-2 border-transparent z-[-1]"
-                }
-              ></div>
-            </Link>
-          );
-        })}
-      </div>
+      <LayoutGroup>
+        <div className="flex gap-6 mt-2">
+          {navLinks.map((link, i) => {
+            let isActive = pathname === link.href;
+            return (
+              <Link
+                key={i}
+                href={link.href}
+                className="relative hover:bg-white/10 rounded transition-all py-2 px-3"
+              >
+                <p className={isActive ? "" : "text-white/70"}>{link.name}</p>
+                {link.id === 1 && link.books !== null && link.books > 0 && (
+                  <div className="absolute top-1 -right-1 bg-yellow-300 text-neutral-800 font-bold rounded-full w-4 h-4 flex items-center justify-center text-xs">
+                    {link.books}
+                  </div>
+                )}
+                {isActive ? (
+                  <motion.div
+                    className="absolute inset-0 border-b-2 border-yellow-300 z-[-1]"
+                    layoutId="sidebar"
+                    transition={{
+                      type: "spring",
+                      stiffness: 350,
+                      damping: 30,
+                    }}
+                  />
+                ) : null}
+              </Link>
+            );
+          })}
+        </div>
+      </LayoutGroup>
     </nav>
   );
 }
